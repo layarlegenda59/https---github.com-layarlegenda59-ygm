726c6a8203a737ca0fbba9b846d325a5e453ddf9
Dadi Rustiawan <rocketfly59@gmail.com>
2025-09-11 08:02:17 +0000

    continue fix

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -8,7 +8,7 @@ interface HeaderProps {
 export function Header({ title, children }: HeaderProps) {
   return (
     <header className="flex items-center gap-4">
-      <SidebarTrigger className="md:hidden" />
+      <SidebarTrigger />
       <h1 className="font-headline text-2xl md:text-3xl font-bold tracking-tight text-foreground">{title}</h1>
       <div className="ml-auto flex items-center gap-2">
         {children}
